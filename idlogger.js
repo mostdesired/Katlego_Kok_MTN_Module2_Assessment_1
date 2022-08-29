7b738b9f2568efc1d1ae4b71a4e24e17d81213fc
mostdesired <31080151+mostdesired@users.noreply.github.com>
2022-08-29 09:13:45 +0200

    Add files via upload

---
+++ idlogger.js
@@ -0,0 +1,5 @@
+var fullname = "katlego kok";
+var institutionName="University of North West";
+var githubName="mostdesired";
+
+console.log("hi,i am " + fullname + " " + "i completed my degree last year at" + " " + institutionName + " " + "and my GitHub username is " + githubName);
